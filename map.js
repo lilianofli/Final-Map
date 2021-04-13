47739437b51eecf2a102423cc0cc8138b6ffb824
lilianofli <79158933+lilianofli@users.noreply.github.com>
2021-04-13 19:10:58 -0400

    Add files via upload

--- map.js
+++ map.js
@@ -6,11 +6,11 @@ var statesData = {"type":"FeatureCollection","features":[
   {"type":"Feature","id":"05","properties":{"name":"Central Africa"},"geometry":{"type":"Polygon","coordinates":[[[22.391, 22.236, -9.492, -19.336]]]}},
   {"type":"Feature","id":"05","properties":{"name":"East Asia"},"geometry":{"type":"Polygon","coordinates":[[[54.52, 151.00, 0.26, 67.85]]]}},
 
-//I don't know the correct way to get the coordinates but I went to https://www.openstreetmap.org > Export > go to the area I want and there are coordinates 
 
 
   var mapboxAccessToken = {your access token here};
 var map = L.map('map').setView([37.8, -96], 4);
+//don't know how to find coordinates 
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
     id: 'mapbox/light-v9',
@@ -19,17 +19,24 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1
 }).addTo(map);
 
-L.geoJson(statesData).addTo(map);
+// from https://leafletjs.com/examples/map-panes/
+var map = L.map('map');
+map.createPane('labels');
+map.getPane('labels').style.zIndex = 650;
 
+var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        attribution: '©OpenStreetMap, ©CartoDB'
+}).addTo(map);
 
+var positronLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+        attribution: '©OpenStreetMap, ©CartoDB',
+        pane: 'labels'
+}).addTo(map);
 
-function getColor(d) {
-  return d > 1000 ? '#800026' :
-         d > 500  ? '#BD0026' :
-         d > 200  ? '#E31A1C' :
-         d > 100  ? '#FC4E2A' :
-         d > 50   ? '#FD8D3C' :
-         d > 20   ? '#FEB24C' :
-         d > 10   ? '#FED976' :
-                    '#FFEDA0';
-}
+var geojson = L.geoJson(GeoJsonData, geoJsonOptions).addTo(map);
+
+geojson.eachLayer(function (layer) {
+  layer.bindPopup(layer.feature.properties.name);
+});
+
+map.fitBounds(geojson.getBounds());
