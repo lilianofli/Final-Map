75a8dc94b52ddd003fc52f9f25440dd811ef416e
Joyce-SinWaiMing <79951315+Joyce-SinWaiMing@users.noreply.github.com>
2021-05-03 04:59:53 -0400

    Add files via upload

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,3 +1,12 @@
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  //document.getElementById("main").style.marginLeft = "0";
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  //document.getElementById("main").style.marginLeft = "250px";
+}
 
 
 //Get the region selection
